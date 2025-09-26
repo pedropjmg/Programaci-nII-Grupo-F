d3badbce696b41f88eac79bda034ebd219283309
pedropjmg <pedritomojgar1001@gmail.com>
2025-09-25 22:48:01 -0400

    Add files via upload

---
+++ doctores.py
@@ -0,0 +1,296 @@
+#Importación de librerías
+import tkinter as tk
+from tkinter import ttk, messagebox
+from datetime import datetime
+#funcion para enmascarar fecha
+def enmascarar_fecha(texto):
+    limpio=''.join(filter(str.isdigit,texto))
+    formato_final=""
+ 
+    if len(limpio)>8:
+        limpio=limpio[:8]
+    if len(limpio)>4:
+        formato_final=f"{limpio[:2]}-{limpio[2:4]}-{limpio[4:]}"
+    elif len(limpio)>2:
+        formato_final=f"{limpio[:2]}-{limpio[2:]}"
+    else:
+        formato_final=limpio
+ 
+    if fechaN.get() !=formato_final:
+        fechaN.delete(0, tk.END)
+        fechaN.insert(0, formato_final)
+ 
+    if len(fechaN.get())==10:
+        fecha_actual=datetime.now().date()
+        fecha_nacimiento=datetime.strptime(fechaN.get(),"%d-%m-%Y").date()
+        edad=fecha_actual.year - fecha_nacimiento.year
+        edadVar.set(edad)
+    else:
+        edadVar.set("")
+    return True
+#GUARDAR ARCHIVOS 
+def guardar_en_archivo():
+    with open("paciente.txt", "w",encoding="utf=8") as archivo:
+        for paciente in paciente_data:
+            archivo.write(f"{paciente['Nombre']}|{paciente['Fecha de Nacimiento']}|{paciente['Edad']}|"
+            f"{paciente['Genero']}|{paciente['Grupo Sanguineo']}|"
+            f"{paciente['Tipo de Seguro']}|{paciente['Centro Medico']}\n")
+            
+            
+            
+     
+#Lista de pacientes
+paciente_data=[]
+#funcion para registrar paciente
+def registrarPaciente():
+    paciente={
+        "Nombre":nombreP.get(),
+        "Fecha de Nacimiento":fechaN.get(),
+        "Edad":edadVar.get(),
+        "Genero":genero.get(),
+        "Grupo Sanguineo":entryGrupoS.get(),
+        "Tipo de Seguro":tipoSeguro.get(),
+        "Centro Medico":centroM.get()
+    }
+#Agregar pacientes a la lista
+    paciente_data.append(paciente)
+#GUARDAR EN EL ARCHIVO
+    guardar_en_archivo()
+#Cargar el Treeview
+    cargar_treeview()
+    
+ 
+def cargar_treeview():
+    #Limpiar el Treeviw
+    for paciente in treeview.get_children():
+        treeview.delete(paciente)
+    #Insertar paciente
+    for i, item in enumerate(paciente_data):
+        treeview.insert(
+            "", "end", iid=str(i),
+            values=(
+                item["Nombre"],
+                item["Fecha de Nacimiento"],
+                item["Edad"],
+                item["Genero"],
+                item["Grupo Sanguineo"],
+                item["Tipo de Seguro"],
+                item["Centro Medico"]
+            )
+        )
+def  cargar_desde_archivo_paciente():
+    try:
+        with open("paciente.txt", "r", encoding="utf-8") as archivo:
+            paciente_data.clear()
+            for linea in archivo:
+                datos=linea.strip().split("|")
+                if len(datos)==7:
+                    paciente={
+                        "Nombre":datos[0],
+                        "Fecha de Nacimiento": datos[1],
+                        "Edad":datos[2],
+                        "Genero":datos[3],
+                        "Grupo Sanguineo": datos[4],
+                        "Tipo de Seguro": datos[4],
+                        "Tipo de seguro": datos[5],
+                        "Centro Medico": datos[6]
+                    }
+                    paciente_data.append(paciente)
+                    cargar_treeview()
+    except FileNotFoundError:
+        open("paciente.txt","w", encoding="utf-8").close()
+#ELIMINAR PACIENTE 
+def eliminarPaciente():
+    seleccionado = treeview.selection()
+    if seleccionado:
+        indice= int(seleccionado[0])
+        id_item = seleccionado[0]
+        if messagebox.askyesno("Eliminar Paciente", f"¿Estas seguro de eliminar el paciente'{treeview.item(id_item,'values')[0]}'?"):
+            del paciente_data[indice]
+            guardar_en_archivo()   #GUARDIA LOS CAMBIOS EN EL ARCHIVO
+            cargar_treeview()
+            messagebox.showinfo("Eliminar paciente","Paciente eliminado exitosamente")
+    else:
+        messagebox.showwarning("Eliminar Paciente","No se ha seleccionado ningun paciente")
+        return
+ 
+ 
+
+# Lista de doctores
+doctor_data = []
+ 
+# Función para registrar doctor
+def registrarDoctor():
+    doctor = {
+        "Nombre": entryNombreD.get(),
+        "Especialidad": especialidadD.get(),
+        "Años de experiencia": entryAñosdeexperiencia.get(),
+        "Genero": genero.get(), 
+        "Hospital": entryHospitalD.get()
+    }
+    doctor_data.append(doctor)
+    cargar_treeview_doctores()
+ 
+# Función para cargar el Treeview de doctores
+def cargar_treeview_doctores():
+    for doctor in treeviewD.get_children():
+        treeviewD.delete(doctor)
+    for i, item in enumerate(doctor_data):
+        treeviewD.insert(
+            "", "end", iid=str(i),
+            values=(
+                item["Nombre"],
+                item["Especialidad"],
+                item["Años de experiencia"],
+                item["Genero"],
+                item["Hospital"]
+            )
+        )
+# Crear ventana principal
+ventanaPrincipal = tk.Tk()
+ventanaPrincipal.title("Libro de Pacientes y Doctores")
+ventanaPrincipal.geometry("890x600")
+# Crear contenedor NoteBook (pestañas)
+pestañas = ttk.Notebook(ventanaPrincipal)
+# Crear frames
+framePacientes = ttk.Frame(pestañas)
+frameDoctores = ttk.Frame(pestañas)
+# Agregar pestañas al NoteBook
+pestañas.add(framePacientes, text="Pacientes")
+pestañas.add(frameDoctores, text="Doctores")
+# Mostrar las pestañas en la ventana
+pestañas.pack(expand=True, fill="both")
+# Nombre
+labelNombre = tk.Label(framePacientes, text=" Nombre Completo:")
+labelNombre.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+nombreP = tk.Entry(framePacientes)
+nombreP.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+# Fecha de nacimiento
+labelFechaN = tk.Label(framePacientes, text=" Fecha de Nacimiento:")
+labelFechaN.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+validacion_fecha=ventanaPrincipal.register(enmascarar_fecha)
+fechaN = ttk.Entry(framePacientes, validate="key", validatecommand=(validacion_fecha, '%P'))
+fechaN.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+# Edad (readonly)
+labelEdad = tk.Label(framePacientes, text=" Edad:")
+labelEdad.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+edadVar=tk.StringVar()
+edadP = tk.Entry(framePacientes, textvariable=edadVar, state="readonly")
+edadP.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+# Género
+labelGenero = tk.Label(framePacientes, text=" Género:")
+labelGenero.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+genero = tk.StringVar()
+genero.set("Masculino") # Valor por defecto
+radioMasculino = ttk.Radiobutton(framePacientes, text="Masculino", variable=genero, value="Masculino")
+radioMasculino.grid(row=3, column=1, padx=5, sticky="w")
+radioFemenino = ttk.Radiobutton(framePacientes, text="Femenino", variable=genero, value="Femenino")
+radioFemenino.grid(row=4, column=1, padx=5, sticky="w")
+# Grupo sanguíneo
+labelGrupoS = tk.Label(framePacientes, text=" Grupo Sanguíneo:")
+labelGrupoS.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+entryGrupoS = tk.Entry(framePacientes)
+entryGrupoS.grid(row=5, column=1, padx=5, pady=5, sticky="w")
+# Tipo de seguro
+labelTipoS = tk.Label(framePacientes, text=" Tipo de Seguro:")
+labelTipoS.grid(row=6, column=0, padx=5, pady=5, sticky="w")
+tipoSeguro = tk.StringVar()
+tipoSeguro.set("Público") # Valor por defecto
+comboTipoS = ttk.Combobox(framePacientes, values=["Público", "Privado","Ninguno"], textvariable=tipoSeguro)
+comboTipoS.grid(row=6, column=1, padx=5, pady=5, sticky="w")
+# Tipo de centro médico
+labelCentroM = tk.Label(framePacientes, text=" Centro Médico:")
+labelCentroM.grid(row=7, column=0, padx=5, pady=5, sticky="w")
+centroM = tk.StringVar()
+centroM.set("Hospital Central") # Valor por defecto
+comboCentroM = ttk.Combobox(framePacientes, values=["Hospital Central", "Clínica Norte","Centro Salud Sur"], textvariable=centroM)
+comboCentroM.grid(row=7, column=1, padx=5, pady=5, sticky="w")
+# Frame para los botones
+btnFrame = tk.Frame(framePacientes)
+btnFrame.grid(row=8, column=1, columnspan=2, pady=5, sticky="w")
+# Botón registrar
+btnRegistrar = tk.Button(btnFrame, text="Registrar", bg="green", fg="white", command=registrarPaciente)
+btnRegistrar.grid(row=0, column=0, padx=5)
+
+# Crera Treeview para mostrar los pacientes
+treeview = ttk.Treeview(framePacientes, columns=("Nombre","FechaN","Edad", "Género", "GrupoS", "TipoS", "CentroM"), show="headings")
+# Definir encabezados
+treeview.heading("Nombre", text="Nombre")
+treeview.heading("FechaN", text="Fecha de Nacimiento")
+treeview.heading("Edad", text="Edad")
+treeview.heading("Género", text="Género")
+treeview.heading("GrupoS", text="Grupo Sanguíneo")
+treeview.heading("TipoS", text="Tipo de Seguro")
+treeview.heading("CentroM", text="Centro Médico")
+#definir ancho de columnas
+treeview.column("Nombre", width=200)    
+treeview.column("FechaN", width=120)
+treeview.column("Edad", width=50)
+treeview.column("Género", width=100)
+treeview.column("GrupoS", width=100)
+treeview.column("TipoS", width=100)
+treeview.column("CentroM", width=150)
+#ubicar el treeview en la cuadricula
+treeview.grid(row=9, column=0, columnspan=4, padx=5, pady=5)
+#Escrolbar vertical
+scrollbar = ttk.Scrollbar(framePacientes, orient="vertical", command=treeview.yview)
+treeview.configure(yscroll=scrollbar.set)
+scrollbar.grid(row=9, column=4, sticky="ns")
+# Nombre
+labelNombreD = tk.Label(frameDoctores, text="Nombre:")
+labelNombreD.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+entryNombreD = tk.Entry(frameDoctores)
+entryNombreD.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+# Especialidad
+labelEspecialidadD = tk.Label(frameDoctores, text="Especialidad:")
+labelEspecialidadD.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+especialidadD = tk.StringVar()
+comboEspecialidadD = ttk.Combobox(frameDoctores, values=["Cardiología", "Dermatología", "Neurología", "Pediatría", "Ginecología", "Ortopedia"], textvariable=especialidadD, state="readonly")
+comboEspecialidadD.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+# Edad
+labelAñosdeexperiencia = tk.Label(frameDoctores, text="Años de experincia:")
+labelAñosdeexperiencia.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+entryAñosdeexperiencia= tk.Spinbox(frameDoctores,from_=0,to=100, width=5)
+entryAñosdeexperiencia.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+# Genero
+labelGeneroD = tk.Label(frameDoctores, text="Genero:")
+labelGeneroD.grid(row=3, column=0, padx=5, pady=5, sticky="w")
+genero = tk.StringVar()
+genero.set("Masculino") # Valor por defecto
+radioMasculino = ttk.Radiobutton(frameDoctores, text="Masculino", variable=genero, value="Masculino")
+radioMasculino.grid(row=3, column=1, padx=5, sticky="w")
+radioFemenino = ttk.Radiobutton(frameDoctores, text="Femenino", variable=genero, value="Femenino")
+radioFemenino.grid(row=3, column=2, padx=5, sticky="w")
+
+
+# HOSPITAL
+labelHospitalD = tk.Label(frameDoctores, text="Hospital:")
+labelHospitalD.grid(row=4, column=0, padx=5, pady=5, sticky="w")
+entryHospitalD = tk.Entry(frameDoctores)
+entryHospitalD.grid(row=4, column=1, padx=5, pady=5, sticky="w")
+# Frame para botones
+btnFrameD = tk.Frame(frameDoctores)
+btnFrameD.grid(row=10, column=1, columnspan=2, pady=5, sticky="w")
+btnRegistrarD = tk.Button(btnFrameD, text="Registrar", bg="green", fg="white", command=registrarDoctor)
+btnRegistrarD.grid(row=10, column=10, padx=5)
+
+# Treeview para mostrar doctores
+treeviewD = ttk.Treeview(frameDoctores, columns=("Nombre", "Especialidad", "Años de experiencia", "Genero", "Hospital"), show="headings")
+treeviewD.heading("Nombre", text="Nombre")
+treeviewD.heading("Especialidad", text="Especialidad")
+treeviewD.heading("Años de experiencia", text="Años de experiencia")
+treeviewD.heading("Genero", text="Generoo")
+treeviewD.heading("Hospital", text="Hospital")
+treeviewD.grid(row=5, column=0, columnspan=4, padx=5, pady=5)
+# Scrollbar vertical para treeview doctores
+scrollbarD = ttk.Scrollbar(frameDoctores, orient="vertical", command=treeviewD.yview)
+treeviewD.configure(yscroll=scrollbarD.set)
+scrollbarD.grid(row=5, column=4, sticky="ns")
+
+
+
+
+
+
+cargar_desde_archivo_paciente()
+ventanaPrincipal.mainloop()
